7f997f38c53ac2df9f3facf576ff9095f3257b8b
root <root@DESKTOP-DHALUET>
2023-06-01 23:50:04 -0500

    archivo 3

---
+++ 3-main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
+    int (*op_func)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        
+            
+    }
+    
+}
